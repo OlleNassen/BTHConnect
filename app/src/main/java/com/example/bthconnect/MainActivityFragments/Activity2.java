89df84010503ffd6ae8b78de9a5ea7b3cd28a27a
sai_nikhil <39191957+nikhil-666@users.noreply.github.com>
2019-12-02 17:41:59 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/bthconnect/MainActivityFragments/Activity2.java
@@ -0,0 +1,29 @@
+package com.example.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+    private Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        button3 = (Button) findViewById(R.id.button2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Activity3 .class);
+        startActivity(intent);
+    }
+}
